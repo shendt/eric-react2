af685a6c3cfd9f5ecbb6fad21519cd083d55ef22
shendt <dshen@umass.edu>
2022-10-05 11:35:04 -0400

    3 files modified

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # eric-react2
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xjq6jz)
+
+- App.js is a read only file containing data passed to the Table component as props
+
+- Create a reusable Table component to display all of the data accordingly.
+
+- You can assume the structure of each item in a data set will be the same
+
+- Each key in data objects should be a table header
+
+- Apply simple styling to make the component look like a table
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-export default function Table() {
+export default function Table(props) {
+  let sample;
   // your code here
+  if (props.data.length > 0) {
+    sample = props.data[0];
+  }
+  let headers;
+  if (sample) {
+    headers = Object.keys(sample);
+  }
+  console.log(headers);
 
   return (
     <table>
-      
+      <thead>
+        <tr>
+          {headers.map((h) => (
+            <td>{h}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((d) => {
+          return (
+            <tr>
+              {headers.map((h) => {
+                return <td>{d[h]}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
-  )
+  );
 }
